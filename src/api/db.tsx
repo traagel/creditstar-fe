3ccf413a5bd34a7d3e0c495346826fe6ee48ae35
traagel <marttraagel@gmail.com>
2023-09-18 09:56:07 +0300

    hardcoding private api and exposing it to the public

--- src/api/db.tsx
+++ src/api/db.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api/v1/data',
+    baseURL: 'https://creditstar-be-production.up.railway.app/api/v1/data',
     headers: {
         'Content-Type': 'application/json',
     },
